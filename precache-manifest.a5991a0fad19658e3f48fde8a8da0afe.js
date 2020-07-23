d550916b5df09213730e0d309874749cc09460d8
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-07-23 13:16:59 +0100

    Updates

---
+++ precache-manifest.a5991a0fad19658e3f48fde8a8da0afe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53463ca267491b69f5c0195409463bf",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "a14501bf8f8fb9db3b4e",
+    "url": "/pokemon-app/static/css/main.2161f67f.chunk.css"
+  },
+  {
+    "revision": "bf41c0a94bf9ba17048d",
+    "url": "/pokemon-app/static/js/2.0241c48e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokemon-app/static/js/2.0241c48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14501bf8f8fb9db3b4e",
+    "url": "/pokemon-app/static/js/main.5ebad7e3.chunk.js"
+  },
+  {
+    "revision": "53b9c226502149a401b8",
+    "url": "/pokemon-app/static/js/runtime-main.9ee58c44.js"
+  },
+  {
+    "revision": "dd3a1a410ab1faaa5cc2f148e2f39361",
+    "url": "/pokemon-app/static/media/loader.dd3a1a41.gif"
+  },
+  {
+    "revision": "b116280030b3c31d7406447a1ebf2835",
+    "url": "/pokemon-app/static/media/pikachu.b1162800.png"
+  }
+]);
